41baf28af4db6cbf2b4ee14790db6c45768a7ff1
Gabe Mendoza <thatguyin@beanie.gg>
2025-05-15 16:30:01 -0500

    fix things

--- __tests__/main_test.go
+++ __tests__/main_test.go
@@ -138,7 +138,9 @@ func TestSourceDirectoryStructure(t *testing.T) {
 	possiblePaths := []string{
 		filepath.Join(rootDir, "MISSION_CONTROL", "source"),
 		filepath.Join(rootDir, "source"),
-		"/home/runner/work/dotfiles/dotfiles/MISSION_CONTROL/source",
+		filepath.Join(rootDir, "MISSION_CONTROL", "source"),
+		"source",                 // Add relative path
+		"MISSION_CONTROL/source", // Add relative path with MISSION_CONTROL
 	}
 
 	var sourcePath string
