cd156fc94b3d5ea5faf39048625b07326fc89d32
Gabe Mendoza <gmendoza@civisanalytics.com>
2025-05-30 11:28:51 -0500

    autodarkmode latte-mocha

--- MISSION_CONTROL/dot_config/nvim/lua/plugins/theme/catppuccin.lua
+++ MISSION_CONTROL/dot_config/nvim/lua/plugins/theme/catppuccin.lua
@@ -66,9 +66,18 @@ return {
     },
   },
   {
-    "LazyVim/LazyVim",
+    "f-person/auto-dark-mode.nvim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.o.background = "dark"
+        vim.cmd("colorscheme catppuccin-mocha")
+      end,
+      set_light_mode = function()
+        vim.o.background = "light"
+        vim.cmd("colorscheme catppuccin-latte")
+      end,
+      fallback = "dark",
     },
-  }, 
+  },
 }
