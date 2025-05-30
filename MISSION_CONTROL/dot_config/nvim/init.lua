c6d456701cb556f9730166fe4ad6403fafbc498e
Gabe Mendoza <thatguyin@beanie.gg>
2025-05-30 08:51:01 -0500

    pid

--- MISSION_CONTROL/dot_config/nvim/init.lua
+++ MISSION_CONTROL/dot_config/nvim/init.lua
@@ -2,5 +2,7 @@
 require("config.lazy")
 
 -- Start MCP server socket for external tool integration
-local socket_path = os.tmpname()  -- Generate a unique temporary file name
-vim.fn.serverstart(socket_path)   -- Start the server with the generated path
+local socket_path = '/tmp/nvim-' .. vim.fn.getpid()
+-- Remove existing socket if it exists
+os.remove(socket_path)
+vim.fn.serverstart(socket_path)
