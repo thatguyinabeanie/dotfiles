cbac897037221621d9ce3180d7f4ac36d46ef456
Gabe Mendoza <thatguyin@beanie.gg>
2025-07-10 10:19:11 -0500

    disable snacks.nvim scope - temp fix replaced by indent-blankline.nvim

---
+++ MISSION_CONTROL/dot_config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,34 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = {
+      enabled = true,
+      char = "│",
+      show_start = true,
+      show_end = false,
+      injected_languages = false,
+      highlight = { "Function", "Label" },
+      priority = 500,
+    },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
+  },
+}
