e67802e9966440066f758723067ba98cba26e113
Gabe Mendoza <thatguyin@beanie.gg>
2025-08-09 01:09:07 -0500

    stuff

--- dot_config/sketchybar/items/spaces.lua
+++ dot_config/sketchybar/items/spaces.lua
@@ -11,24 +11,23 @@ for i = 1, 10, 1 do
 		icon = {
 			font = { family = settings.font.numbers },
 			string = i,
-			padding_left = 15,
-			padding_right = 8,
+			padding_left = 20,
+			padding_right = 12,
 			color = colors.white,
 			highlight_color = colors.red,
 		},
 		label = {
-			padding_right = 20,
+			padding_right = 25,
 			color = colors.grey,
 			highlight_color = colors.white,
 			font = "sketchybar-app-font:Regular:16.0",
 			y_offset = -1,
 		},
-		padding_right = 1,
-		padding_left = 1,
+		padding_right = 2,
+		padding_left = 2,
 		background = {
 			color = colors.bg1,
 			border_width = 1,
-			height = 26,
 			border_color = colors.black,
 		},
 		popup = { background = { border_width = 5, border_color = colors.black } },
@@ -41,7 +40,6 @@ for i = 1, 10, 1 do
 		background = {
 			color = colors.transparent,
 			border_color = colors.bg2,
-			height = 28,
 			border_width = 2,
 		},
 	})
