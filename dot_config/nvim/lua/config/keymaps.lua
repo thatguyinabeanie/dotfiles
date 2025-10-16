774d867a9377a2fb9d3efbe3c3e9ae4d95e2127e
Gabe Mendoza <thatguyin@beanie.gg>
2025-10-15 21:37:36 -0500

    lazy

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -19,6 +19,7 @@
 vim.keymap.set("n", "<leader>le", "<cmd>LazyExtras<cr>", { desc = "Lazy Extras" })
 vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "Lazy Git" })
 vim.keymap.set("n", "<leader>ld", "<cmd>LazyDocker<cr>", { desc = "Lazy Docker" })
+vim.keymap.set("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
 -- Projects picker
 vim.keymap.set("n", "<leader>fp", function()
