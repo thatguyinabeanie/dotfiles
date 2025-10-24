f704091bf868c62fc238d9972c0d94997fc50285
Gabe Mendoza <thatguyin@beanie.gg>
2025-10-24 14:40:48 -0500

    undotree

--- dot_config/nvim/lua/plugins/core/undotree.lua
+++ dot_config/nvim/lua/plugins/core/undotree.lua
@@ -3,6 +3,11 @@ return {
     "mbbill/undotree",
     event = "VeryLazy",
     config = function()
+      -- Set up persistent undo directory
+      local undodir = vim.fn.expand("~/.undotree")
+      vim.opt.undodir = undodir
+      vim.opt.undofile = true
+
       vim.keymap.set("n", "<leader>uu", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
     end,
   },
