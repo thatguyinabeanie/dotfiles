5cbbc9d2da2a5f780ae9a1adf458b0e4ceae32dd
Gabe Mendoza <thatguyin@beanie.gg>
2025-11-25 13:01:38 -0600

    feat: add lazygit.nvim plugin and fix tmux navigation in terminal buffers

--- dot_config/nvim/lua/plugins/utilities/lazydocker.lua
+++ dot_config/nvim/lua/plugins/utilities/lazydocker.lua
@@ -2,30 +2,31 @@
 return {
   "crnvl96/lazydocker.nvim",
   keys = {
-    { "<leader>l", "", desc = "+LazyDocker" },
+    { "<leader>l", "", desc = "+Lazy Tools" },
     {
       "<leader>ld",
       "<cmd>lua require('lazydocker').toggle({ engine = 'docker' })<cr>",
       mode = { "n", "t" },
       desc = "LazyDocker (Docker)",
     },
-    {
-      "<leader>lp",
-      "<cmd>lua require('lazydocker').toggle({ engine = 'podman' })<cr>",
-      mode = { "n", "t" },
-      desc = "LazyDocker (Podman)",
-    },
   },
 
   opts = {
     window = {
       settings = {
-        width = 0.618, -- Golden ratio percentage of screen width
-        height = 0.618, -- Golden ratio percentage of screen height
+        width = 0.95, -- 95% of screen width for larger view
+        height = 0.95, -- 95% of screen height for larger view
         border = "rounded",
         relative = "editor",
       },
     },
+    -- Pass through navigation keys to vim-tmux-navigator
+    on_open = function(term)
+      vim.api.nvim_buf_set_keymap(term.buf, "t", "<C-h>", "<C-\\><C-n><C-h>", { silent = true })
+      vim.api.nvim_buf_set_keymap(term.buf, "t", "<C-j>", "<C-\\><C-n><C-j>", { silent = true })
+      vim.api.nvim_buf_set_keymap(term.buf, "t", "<C-k>", "<C-\\><C-n><C-k>", { silent = true })
+      vim.api.nvim_buf_set_keymap(term.buf, "t", "<C-l>", "<C-\\><C-n><C-l>", { silent = true })
+    end,
   },
 
   config = function(_, opts)
