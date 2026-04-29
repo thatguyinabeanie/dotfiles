44c79e7cf284c8e8e01e7c47f334d2c4be00d039
Gabe Mendoza <thatguyin@beanie.gg>
2026-04-29 17:04:26 -0500

    add Nightfall LaunchAgent, pin Elgato Studio to workspace E, update tools and configs

--- dot_config/nvim/lua/plugins/core/blink.lua
+++ dot_config/nvim/lua/plugins/core/blink.lua
@@ -1,3 +1,8 @@
 return {
-  { "saghen/blink.lib" },
+  -- blink.cmp v2 requires blink.lib as a separate dependency
+  { "saghen/blink.lib", lazy = true },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "saghen/blink.lib" },
+  },
 }
